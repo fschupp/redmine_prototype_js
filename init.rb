a498706dc10765bf6257fff43250fc418de76112
Felix Schupp <fschupp@outlook.com>
2015-01-06 11:31:19 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,11 +1,14 @@
 require 'redmine'
 require 'redmine_prototype_js/hooks'
+
 Redmine::Plugin.register :redmine_prototype_js do
-name 'Redmine Prototype JS plugin'
-description 'This plugin adds the Protoype JS library to your Redmine instance'
-author 'Felix Schupp'
-author_url 'mailto:fschupp@outlook.com'
-version '0.0.1'
-url 'https://github.com/fschupp/redmine_prototype_js'
-requires_redmine :version_or_higher => '2.6.0'
+  name 'Redmine Prototype JS plugin'
+  description 'This plugin adds the Protoype JS library to your Redmine instance'
+  author 'Felix Schupp'
+  author_url 'mailto:fschupp@outlook.com'
+  version '0.0.2'
+  url 'https://github.com/fschupp/redmine_prototype_js'
+  requires_redmine :version_or_higher => '2.6.0'
+
+  settings default: {empty: true}, partial: 'settings/redmine_prototype_js'
 end
