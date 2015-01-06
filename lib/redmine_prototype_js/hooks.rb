a997177a9db113a1a07cec0f739d75a1094c11e2
fschupp <fschupp@outlook.com>
2015-01-06 10:29:09 +0100

    Update hooks.rb

--- lib/redmine_prototype_js/hooks.rb
+++ lib/redmine_prototype_js/hooks.rb
@@ -1,10 +1,10 @@
 module RedminePrototypeJs
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context)
-        javascript_include_tag("controls.js", :plugin => "redmine_prototype_js")
-        javascript_include_tag("dragdrop.js", :plugin => "redmine_prototype_js")
-        javascript_include_tag("effects.js", :plugin => "redmine_prototype_js")
-        javascript_include_tag("prototype.js", :plugin => "redmine_prototype_js")
+        javascript_include_tag("controls.js", :plugin => "redmine_prototype_js") +
+        javascript_include_tag("dragdrop.js", :plugin => "redmine_prototype_js") +
+        javascript_include_tag("effects.js", :plugin => "redmine_prototype_js") +
+        javascript_include_tag("prototype.js", :plugin => "redmine_prototype_js") +
         javascript_include_tag("prototype_ujs.js", :plugin => "redmine_prototype_js")
     end
   end
