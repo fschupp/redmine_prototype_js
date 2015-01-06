30757d44005a8358c83923caf102ea55806beafe
Felix Schupp <fschupp@outlook.com>
2015-01-06 11:41:04 +0100

    Update hooks.rb

--- lib/redmine_prototype_js/hooks.rb
+++ lib/redmine_prototype_js/hooks.rb
@@ -1,22 +1,5 @@
 module RedminePrototypeJs
   class Hooks < Redmine::Hook::ViewListener
-    def view_layouts_base_html_head(context)
-      
-      <% if settings['rmprotojs_enable_control'] %>
-        javascript_include_tag("controls.js", :plugin => "redmine_prototype_js") +
-      <% end %>
-      <% if settings['rmprotojs_enable_dragdrop'] %>
-        javascript_include_tag("dragdrop.js", :plugin => "redmine_prototype_js") +
-      <% end %>
-      <% if settings['rmprotojs_enable_effects'] %>
-        javascript_include_tag("effects.js", :plugin => "redmine_prototype_js") +
-      <% end %>
-      <% if settings['rmprotojs_enable_prototype'] %>
-        javascript_include_tag("prototype.js", :plugin => "redmine_prototype_js") +
-      <% end %>
-      <% if settings['rmprotojs_enable_prototype_ujs'] %>#
-        javascript_include_tag("prototype_ujs.js", :plugin => "redmine_prototype_js")
-      <% end %>
-    end
+     render_on(:view_layouts_base_html_head, :partial => "hooks/redmine_prototype_js/html_head")
   end
 end
